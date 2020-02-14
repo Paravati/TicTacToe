ce779fd4b6755856cbdea41309c9f334e8ae7fe5
Iwanek <iwanek@infoscan.pl>
2020-02-14 19:55:01 +0100

    added option of choosing symbol by user

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        char symbolOfUser = 'a';
         System.out.println("Gra kolko i krzyzyk z komputerem.\nAby rozpocząć grę kliknij 1.");
         Scanner in = new Scanner(System.in);
-        System.out.println("Podaj liczbe (rozmiar planszy do gry)");
+        System.out.print("Podaj liczbe (rozmiar planszy do gry): ");
         int sizeOfGameBoard = in.nextInt();
         char[][] gameBoard = new char[sizeOfGameBoard][sizeOfGameBoard];
 
+        while(true){
+            System.out.print("Wybierz symbol: kółko lub krzyżyk - ");
+            symbolOfUser = in.next().charAt(0);
+            if(symbolOfUser == 'o' || symbolOfUser == 'x')
+                break;
+            System.out.println("Podano niepoprawny symbol. Musisz spróbować jeszcze raz.");
+        }
+
         for(int i = 0; i<sizeOfGameBoard; i++){
             for(int j = 0; j< sizeOfGameBoard; j++){
-                gameBoard[i][j] = 'x';
+                gameBoard[i][j] = symbolOfUser;
             }
         }
 
